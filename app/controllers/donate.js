459a1db65afa594cdbc6fd484ee5dc63cb49a348
Matthew Congrove <mattcongrove@gmail.com>
2013-01-14 15:43:57 -0500

    Closes #82.

--- app/controllers/donate.js
+++ app/controllers/donate.js
@@ -2,32 +2,24 @@ var APP = require("core");
 
 var CONFIG = arguments[0];
 
-$.init = function() {
-	APP.log("debug", "donate.init | " + JSON.stringify(CONFIG));
-	
-	$.NavigationBar.Wrapper.backgroundColor = APP.Settings.colors.primary || "#000";
-	$.NavigationBar.right.visible			= true;
-	$.NavigationBar.rightImage.image		= "/images/settings.png";
-	
-	$.content.url = "/data/" + CONFIG.file;
-};
+APP.log("debug", "donate | " + JSON.stringify(CONFIG));
 
-// Event listeners
+$.heading.text				= CONFIG.heading;
+$.heading.color				= APP.Settings.colors.primary || "#666";
+$.text.text					= CONFIG.text;
+$.button.backgroundColor	= APP.Settings.colors.primary || "#FFF";
+$.button.color				= APP.Settings.colors.text || "#FFF";
+
+$.NavigationBar.Wrapper.backgroundColor = APP.Settings.colors.primary || "#000";
+$.NavigationBar.right.visible			= true;
+$.NavigationBar.rightImage.image		= "/images/settings.png";
+
 $.NavigationBar.right.addEventListener("click", function(_event) {
 	APP.openSettings();
 });
 
-Ti.App.addEventListener("APP:openLink", function(_event) {
+$.button.addEventListener("click", function(_event) {
 	APP.log("debug", "donate @openLink");
 	
 	Ti.Platform.openURL(_event.url);
 });
-
-Ti.App.addEventListener("APP:openTab", function(_event) {
-	APP.log("debug", "donate @openTab");
-	
-	APP.handleNavigation(_event.index);
-});
-
-// Kick off the init
-$.init();
