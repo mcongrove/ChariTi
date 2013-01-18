55b9d077250aa7d608dd813b2a148b02bb6a69f1
Matthew Congrove <mattcongrove@gmail.com>
2013-01-18 17:03:51 -0500

    Fixes #28, adds pull-to-refresh on Blog, Facebook, News, Twitter.

--- app/controllers/twitter.js
+++ app/controllers/twitter.js
@@ -1,8 +1,12 @@
-var APP = require("core");
-var UTIL = require("utilities");
-var MODEL = require("models/twitter");
+var APP		= require("core");
+var UTIL	= require("utilities");
+var MODEL	= require("models/twitter");
 
-var CONFIG = arguments[0];
+var CONFIG	= arguments[0];
+
+var offset			= 0;
+var refreshLoading	= false;
+var refreshEngaged	= false;
 
 $.init = function() {
 	APP.log("debug", "twitter.init | " + JSON.stringify(CONFIG));
@@ -20,6 +24,14 @@ $.init = function() {
 			$.handleData(MODEL.getTweets());
 		}
 	});
+	
+	var initRefresh = setInterval(function(_event) {
+		if(offset > 30) {
+			clearInterval(initRefresh);
+		}
+		
+		$.container.scrollTo(0, 60);
+	}, 100);
 };
 
 $.handleData = function(_data) {
@@ -46,6 +58,71 @@ $.handleData = function(_data) {
 // Event listeners
 $.NavigationBar.right.addEventListener("click", function(_event) {
 	APP.openSettings();
+});
+
+$.container.addEventListener("scroll", function(_event) {
+	if(_event.y !== null) {
+		offset = _event.y;
+		
+		if(!refreshLoading) {
+			var transform	= Ti.UI.create2DMatrix();
+			
+			if(offset < 0) {
+				if(refreshEngaged == false) {
+					$.refreshLabel.text = "Release to reload...";
+					
+					transform = transform.rotate(-180);
+				
+					$.refreshArrow.animate({
+						transform: transform,
+						duration: 100
+					});
+					
+					refreshEngaged = true;
+				}
+			} else {
+				if(refreshEngaged == true) {
+					$.refreshLabel.text = "Pull down to update...";
+					
+					$.refreshArrow.animate({
+						transform: transform,
+						duration: 100
+					});
+					
+					refreshEngaged = false;
+				}
+			}
+		}
+	}
+});
+
+$.container.addEventListener("dragend", function(_event) {
+	if(offset < 0) {
+		refreshLoading = true;
+		
+		$.refreshLabel.text			= "Loading new content...";
+		$.refreshArrow.visible		= false;
+		$.refreshLoading.visible	= true;
+		
+		$.refreshLoading.start();
+		
+		MODEL.fetch({
+			url: "https://api.twitter.com/1/statuses/user_timeline.json?trim_user=true&include_rts=false&exclude_replies=true&count=50&screen_name=" + CONFIG.username,
+			cache: 0,
+			callback: function() {
+				$.handleData(MODEL.getTweets());
+				
+				refreshLoading = false;
+				
+				$.container.scrollTo(0, 60);
+				
+				$.refreshArrow.visible		= true;
+				$.refreshLoading.visible	= false;
+			}
+		});
+	} else if(offset < 60 && !refreshLoading) {
+		$.container.scrollTo(0, 60);	
+	}
 });
 
 // Kick off the init
