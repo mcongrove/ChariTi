1812723ac03b0e1241fb7331a16617bc76f99668
Matthew Congrove <mattcongrove@gmail.com>
2013-06-18 15:29:34 -0400

    Initial styling work

--- app/controllers/text.js
+++ app/controllers/text.js
@@ -5,10 +5,9 @@ var CONFIG = arguments[0];
 APP.log("debug", "text | " + JSON.stringify(CONFIG));
 
 $.heading.text = CONFIG.heading;
-$.heading.color = APP.Settings.colors.primary || "#666";
 $.text.text = CONFIG.text;
 
-$.NavigationBar.setBackgroundColor(APP.Settings.colors.primary || "#000");
+$.NavigationBar.setBackgroundColor(APP.Settings.colors.primary || "#FFF");
 
 if(CONFIG.isChild === true) {
 	$.NavigationBar.showBack();
--- app/widgets/com.chariti.navigationBar/controllers/widget.js
+++ app/widgets/com.chariti.navigationBar/controllers/widget.js
@@ -25,16 +25,11 @@ if(CONFIG.image) {
 		right: "58dp",
 		height: "46dp",
 		font: {
-			fontSize: "22dp",
-			fontWeight: "bold"
+			fontSize: "18dp",
+			fontFamily: "HelveticaNeue-Medium"
 		},
 		color: "#FFF",
 		textAlign: "center",
-		shadowColor: "#000",
-		shadowOffset: {
-			x: "0dp",
-			y: "1dp"
-		},
 		text: CONFIG.text ? CONFIG.text : ""
 	});
 }
