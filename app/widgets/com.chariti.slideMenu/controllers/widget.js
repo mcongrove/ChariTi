e5d909d7d2fd863debb40bf82077acaab4934d6b
Matthew Congrove <mattcongrove@gmail.com>
2013-10-03 16:04:18 -0400

    Slide menu optimizations

--- app/widgets/com.chariti.slideMenu/controllers/widget.js
+++ app/widgets/com.chariti.slideMenu/controllers/widget.js
@@ -3,6 +3,13 @@ var APP = require("core");
 $.init = function(_params) {
 	$.tabs = [];
 	
+	// Add the Settings tab
+	_params.tabs.push({
+		id: "settings",
+		image: WPATH("images/settings.png"),
+		title: "Settings"
+	});
+	
 	for(var i = 0; i < _params.tabs.length; i++) {
 		var tab = Ti.UI.createTableViewRow({
 			id: _params.tabs[i].id,
@@ -11,20 +18,13 @@ $.init = function(_params) {
 			backgroundSelectedColor: "#222",
 			selectedBackgroundColor: "#222"
 		});
-		
-		var iconWrapper = Ti.UI.createView({
-			width: "28dp",
-			height: "28dp",
-			top: "9dp",
-			left: "7dp",
-			touchEnabled: false
-		});
 
 		var icon = Ti.UI.createImageView({
 			image: _params.tabs[i].image,
-			width: Ti.UI.SIZE,
-			height: "28dp",
-			top: 0,
+			width: "21dp",
+			height: "21dp",
+			top: "13dp",
+			left: "13dp",
 			touchEnabled: false,
 			preventDefaultImage: true
 		});
@@ -33,7 +33,7 @@ $.init = function(_params) {
 			text: _params.tabs[i].title,
 			top: "0dp",
 			left: "47dp",
-			right: "7dp",
+			right: "13dp",
 			height: "47dp",
 			font: {
 				fontSize: "16dp",
@@ -43,55 +43,13 @@ $.init = function(_params) {
 			touchEnabled: false
 		});
 		
-		iconWrapper.add(icon);
-		tab.add(iconWrapper);
+		tab.add(icon);
 		tab.add(label);
 		
 		$.tabs.push(tab);
 	}
 	
 	$.Tabs.setData($.tabs);
-	
-	$.createSettings();
-};
-
-$.createSettings = function() {
-	var tab = Ti.UI.createTableViewRow({
-		id: "settings",
-		height: "47dp",
-		backgroundcolor: "#111",
-		backgroundSelectedColor: "#222",
-		selectedBackgroundColor: "#222"
-	});
-	
-	var icon = Ti.UI.createImageView({
-		image: WPATH("images/settings.png"),
-		width: "28dp",
-		height: "28dp",
-		top: "9dp",
-		left: "7dp",
-		touchEnabled: false,
-		preventDefaultImage: true
-	});
-	
-	var label = Ti.UI.createLabel({
-		text: "Settings",
-		top: "0dp",
-		left: "47dp",
-		right: "7dp",
-		height: "47dp",
-		font: {
-			fontSize: "16dp",
-			fontFamily: "HelveticaNeue-Light"
-		},
-		color: "#FFF",
-		touchEnabled: false
-	});
-	
-	tab.add(icon);
-	tab.add(label);
-	
-	$.Tabs.appendRow(tab);
 };
 
 $.clear = function() {
