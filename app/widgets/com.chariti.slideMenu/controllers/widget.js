9d123ecfa7cc3315b54c4c47b7840e9872f30b4a
Aaron Hartwig <hartwig9@uwm.edu>
2013-10-29 21:56:27 -0500

    Menu Headers
    Utilizes TableViewSections to create a Menu Header system

--- app/widgets/com.chariti.slideMenu/controllers/widget.js
+++ app/widgets/com.chariti.slideMenu/controllers/widget.js
@@ -1,4 +1,5 @@
-var APP = require("core");
+var APP = require("core"),
+	sections = [];
 
 $.init = function(_params) {
 	$.tabs = [];
@@ -10,103 +11,98 @@ $.init = function(_params) {
 		title: "Settings"
 	});
 	
+	//creates a TableViewSection for each tab with a menuHeader property
+	$.buildSections(_params.tabs);
+	var currentSection = -1;
+
 	for(var i = 0; i < _params.tabs.length; i++) {
-		//assigns special menuHeader styling
+		//iterates through the created sections
 		if(_params.tabs[i].menuHeader) {
+			currentSection++;
+		}
 
-			var tab = Ti.UI.createTableViewRow({
-				height : "68dp",
-				backgroundcolor: "#111",
-				backgroundSelectedColor: "#222",
-				selectedBackgroundColor: "#222",
-				isHeader : true
-			});
+		var tab = Ti.UI.createTableViewRow({
+			id: _params.tabs[i].id,
+			height: "47dp",
+			backgroundcolor: "#111",
+			backgroundSelectedColor: "#222",
+			selectedBackgroundColor: "#222"
+		});
 
-			var header = Ti.UI.createLabel({
-				text : _params.tabs[i].menuHeader,
-				top : "0dp",
-				left : "6dp",
-				height : "20dp",
-				font : {
-					fontSize : "13dp",
-					fontWeight : "HelveticaNeue-Light"
-				},
-				color : "#FFF",
-			
-				touchEnabled : false,
-				isHeader : true
-			});
-
-			var icon = Ti.UI.createImageView({
-				image: _params.tabs[i].image,
-				width: "21dp",
-				height: "21dp",
-				top: "33dp",
-				left: "13dp",
-				touchEnabled: false,
-				preventDefaultImage: true
-			});
-			
-			var label = Ti.UI.createLabel({
-				text: _params.tabs[i].title,
-				top: "20dp",
-				left: "47dp",
-				right: "13dp",
-				height: "47dp",
-				font: {
-					fontSize: "16dp",
-					fontFamily: "HelveticaNeue-Light"
-				},
-				color: "#FFF",
-				touchEnabled: false
-			});
-			
-			tab.add(header);
-			tab.add(icon);
-			tab.add(label);
-			
-			$.tabs.push(tab);
-		} else {
-			var tab = Ti.UI.createTableViewRow({
-				id: _params.tabs[i].id,
-				height: "47dp",
-				backgroundcolor: "#111",
-				backgroundSelectedColor: "#222",
-				selectedBackgroundColor: "#222"
-			});
-
-			var icon = Ti.UI.createImageView({
-				image: _params.tabs[i].image,
-				width: "21dp",
-				height: "21dp",
-				top: "13dp",
-				left: "13dp",
-				touchEnabled: false,
-				preventDefaultImage: true
-			});
-			
-			var label = Ti.UI.createLabel({
-				text: _params.tabs[i].title,
-				top: "0dp",
-				left: "47dp",
-				right: "13dp",
-				height: "47dp",
-				font: {
-					fontSize: "16dp",
-					fontFamily: "HelveticaNeue-Light"
-				},
-				color: "#FFF",
-				touchEnabled: false
-			});
-			
-			tab.add(icon);
-			tab.add(label);
-			
-			$.tabs.push(tab);
+		var icon = Ti.UI.createImageView({
+			image: _params.tabs[i].image,
+			width: "21dp",
+			height: "21dp",
+			top: "13dp",
+			left: "13dp",
+			touchEnabled: false,
+			preventDefaultImage: true
+		});
+		
+		var label = Ti.UI.createLabel({
+			text: _params.tabs[i].title,
+			top: "0dp",
+			left: "47dp",
+			right: "13dp",
+			height: "47dp",
+			font: {
+				fontSize: "16dp",
+				fontFamily: "HelveticaNeue-Light"
+			},
+			color: "#FFF",
+			touchEnabled: false
+		});
+		
+		tab.add(icon);
+		tab.add(label);
+		sections[currentSection].add(tab);
+		
+		//If the last tab has been created and added to a section or
+		//the next tab is a new header, append the current section to the TableView
+		if(i + 1 !== _params.tabs.length){
+			if(_params.tabs[i+1].menuHeader){
+				$.Tabs.appendSection(sections[currentSection]);
+			}
+		}else{
+			$.Tabs.appendSection(sections[currentSection]);
 		}
 	}
-	
-	$.Tabs.setData($.tabs);
+};
+
+$.buildSections = function(_tabs){
+	for(var i = 0; i < _tabs.length; i++){
+
+		//assigns special menuHeader styling
+		if(_tabs[i].menuHeader){
+
+			var section = Ti.UI.createTableViewSection();
+
+			var header = Ti.UI.createView({
+				top : "0dp",
+				height : "20dp",
+				width: Ti.UI.FILL,
+				backgroundColor: "#327B9F"
+			});
+			
+			var headerText = Ti.UI.createLabel({
+				text: _tabs[i].menuHeader,
+				top: "2dp",
+				left: "13dp",
+				font : {
+					fontSize : "12dp",
+					fontWeight : "HelveticaNeue-Light"
+				},
+				color: "#FFF",
+				touchEnabled : false,
+				verticalAlignment: Titanium.UI.TEXT_VERTICAL_ALIGNMENT_CENTER,
+				isHeader : true
+			});
+			
+			header.add(headerText);
+			section.headerView = header;
+			sections.push(section);
+		}
+	}
 };
 
 $.clear = function() {
